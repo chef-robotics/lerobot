3f1b8dde3ff1f63bc94c6e015a88b9617e1c6aed
Somudro Gupta <somudro@chefrobotics.ai>
2026-01-21 19:13:24 -0800

    added pathlib

--- lerobot/scripts/control_robot.py
+++ lerobot/scripts/control_robot.py
@@ -138,6 +138,7 @@ import logging
 import time
 from dataclasses import asdict
 from pprint import pformat
+from pathlib import Path
 
 # from safetensors.torch import load_file, save_file
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
